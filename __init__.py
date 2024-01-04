21049bb743d69ffe1ecd5f3ad079e94186959f79
Hina Chen <hinablue@gmail.com>
2024-01-04 22:32:28 +0800

    Add cleanup method for __init__

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
-# Title: ComfyUI Install Customs Nodes and javascript files
+# Title: ComfyUI Install Customs Nodes
 # Author: Hina Chen
-# Version: 2023.11.30
+# Version: 2024.01.04
 
 import os
 import filecmp
@@ -15,31 +15,15 @@ WEB_DIRECTORY = "./web"
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS', 'WEB_DIRECTORY']
 
-# # ComfyUI folders web
-# folder_web = os.path.join(os.path.dirname(os.path.realpath(__main__.__file__)), "web")
-# extensions_folder = os.path.join(folder_web, 'extensions', 'ComfyUI_3dPoseEditor')
-# web_folder = os.path.join(extensions_folder, 'web')
-# assets_folder = os.path.join(extensions_folder, 'assets')
-# webSource_folder = os.path.join(os.path.dirname(os.path.realpath(__file__)), "web")
-# assetsSource_folder = os.path.join(os.path.dirname(os.path.realpath(__file__)), "assets")
+# # Cleanup old extension folder
+folder_web = os.path.join(os.path.dirname(os.path.realpath(__main__.__file__)), "web")
+extensions_folder = os.path.join(folder_web, 'extensions', 'ComfyUI_3dPoseEditor')
 
-# def update_web():
-#     if os.path.exists(extensions_folder):
-#         shutil.rmtree(extensions_folder)
+def cleanup():
+    if os.path.exists(extensions_folder):
+        shutil.rmtree(extensions_folder)
+        print('\033[34m3D OpenPose Editor: \033[92mRemoved old extension folder\033[0m')
 
-#     print("[3D OpenPose Editor] Creating frontend extension folder: " + extensions_folder)
-#     os.mkdir(extensions_folder)
-
-#     print('[3D OpenPose Editor] Copy web folder')
-#     if os.path.exists(web_folder):
-#         shutil.rmtree(web_folder)
-#     copy_tree(webSource_folder, web_folder)
-
-#     print('[3D OpenPose Editor] Copy assets folder')
-#     if os.path.exists(assets_folder):
-#         shutil.rmtree(assets_folder)
-#     copy_tree(assetsSource_folder, assets_folder)
-
-# update_web()
+cleanup()
 
 print('\033[34m3D OpenPose Editor: \033[92mLoaded\033[0m')
